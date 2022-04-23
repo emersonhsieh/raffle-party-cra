90f0b169927580ab82fba505b388e46608989a21
Emerson Hsieh <emersonhsieh@berkeley.edu>
2022-04-23 21:23:52 +0200

    feat: margin top for container

--- src/screens/AddToPool.tsx
+++ src/screens/AddToPool.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Header title="Raffle Party" activeTab="ADD_TO_POOL" />
       <Container>
         <Row>
-          <Col md={12} className="mt-2">
+          <Col md={12} style={{ marginTop: 36 }}>
             <h3>Add To Pool</h3>
             <p className="lead">Hello world</p>
           </Col>
