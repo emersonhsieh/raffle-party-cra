aa2784af0a34c8d4af2952349d80563d2184cdce
Emerson Hsieh <emersonhsieh@berkeley.edu>
2022-04-24 03:08:11 +0200

    feat: home sample raffle

--- src/screens/DisplayRaffle.tsx
+++ src/screens/DisplayRaffle.tsx
@@ -1,3 +1,6 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -5,6 +8,14 @@ import Col from "react-bootstrap/Col";
 import Header from "../components/Header";
 
 export default function Home() {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const raffleId = searchParams.get("id");
+
+    // fetch raffle information
+  }, []);
+
   return (
     <>
       <Header title="Raffle Party" activeTab="ADD_TO_POOL" />
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,20 @@
+import React from "react";
+import { useEffect } from "react";
+
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import Header from "../components/Header";
 
 export default function Home() {
+  useEffect(() => {
+    // Fetch all raffles
+  }, []);
+
   return (
     <>
       <Header title="Raffle Party" activeTab="HOME" />
@@ -12,7 +22,25 @@ export default function Home() {
         <Row>
           <Col md={12} style={{ marginTop: 36 }}>
             <h3>Home</h3>
-            <p className="lead">Hello world</p>
+            <p className="lead">Available Raffles</p>
+          </Col>
+        </Row>
+      </Container>
+      {/* List all raffles */}
+      <Container>
+        <Row>
+          <Col md={3} style={{ marginTop: 36 }}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://amsterdam.ethglobal.com/img/windmill.svg"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>Card Content.</Card.Text>
+                <Button variant="primary">Raffle page</Button>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
